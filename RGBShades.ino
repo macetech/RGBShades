37e17bd4412d4a2ea25ea0c735493a4bb71cd6af
Noah <nwsd02@gmail.com>
2017-12-18 00:01:21 -0800

    radiating rainbow colors from center

--- RGBShades.ino
+++ RGBShades.ino
@@ -56,7 +56,9 @@ void setup() {
 }
 
 // list of functions that will be displayed
-functionList effectList[] = {threeSine,
+functionList effectList[] = {radiateCenter,
+                             hueRotation,
+                             threeSine,
                              threeDee,
                              scrollTextOne,
                              plasma,
